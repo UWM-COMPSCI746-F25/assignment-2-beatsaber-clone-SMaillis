ac93fbfa8ae28e895ef8e0c26125029a73309a42
SMaillis <153215256+SMaillis@users.noreply.github.com>
2025-10-20 18:21:21 -0500

    Part 6 done
    Now every time the timer is about to run out 1 cube is spawned, its color is random if its red or blue

--- Scripts/BlueCube.gd
+++ Scripts/BlueCube.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
 @export var dir = Vector3(0,0,1)
-@export var speed = 5
+@export var speed = 5.0
 
 func _process(delta):
 	global_position += dir * (speed * delta)
-	if global_position.z > 5:
+	if global_position.z > 5.0:
 		queue_free()
--- Scripts/BoxSpawner.gd
+++ Scripts/BoxSpawner.gd
@@ -2,8 +2,27 @@ extends Node3D
 
 @onready var RedBox = load("res://Scenes/RedBox.tscn")
 @onready var BlueBox = load("res://Scenes/BlueBox.tscn")
+@onready var time = $Timer
+var spawned = false
 
-
+func _process(delta):
+	print(time.time_left)
+	if time.time_left < 0.05:
+		if not spawned:
+			var block_spawn = randi() % 2
+			if block_spawn == 0:
+				var instance = BlueBox.instantiate()
+				add_child(instance)
+			else:
+				var instance = RedBox.instantiate()
+				add_child(instance)
+			spawned = true
+			var new_time = randf_range(0.5, 2)
+			print("new_time: ", new_time)
+			time.set_wait_time(new_time)
+			
+	else:
+		spawned = false
 
 func _on_left_hand_button_pressed(name):
 	if name == "trigger_click":
